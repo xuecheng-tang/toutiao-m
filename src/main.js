b4815edbf6c7c86da1f79deab6c98d9f67079ea4
唐雪成 <979937561@qq.com>
2022-12-05 10:17:35 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 // 加载全局样式
 import './styles/index.less'
+// 加载Vant核心组件库
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
